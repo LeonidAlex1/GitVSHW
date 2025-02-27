df157d4f966ec99b3c4d84e5525f03c0838bf77b
Leonid <leonid.alyeksyeyev@gmail.com>
2025-02-27 18:43:26 +0200

    second commit

--- GitVSHW.cpp
+++ GitVSHW.cpp
@@ -17,3 +17,41 @@ struct Book {
     double rating;
 };
 
+class Library {
+private:
+    vector<Book> books;
+    int nextId;
+
+public:
+    Library() : nextId(1) {}
+
+    void addBook(const string& title, const string& author, const string& genre, int year, bool available, double rating) {
+        books.push_back({ nextId++, title, author, genre, year, available, rating });
+    }
+
+    void removeBook(int id) {
+        books.erase(remove_if(books.begin(), books.end(), [id](const Book& b) { return b.id == id; }), books.end());
+    }
+
+    void updateBook(int id, const string& title, const string& author, const string& genre, int year, bool available, double rating) {
+        for (auto& book : books) {
+            if (book.id == id) {
+                book.title = title;
+                book.author = author;
+                book.genre = genre;
+                book.year = year;
+                book.available = available;
+                book.rating = rating;
+                break;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+};
